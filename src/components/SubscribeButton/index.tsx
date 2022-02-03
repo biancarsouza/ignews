a9dee5ab7cdb72e18e8d5daf2c1da1f85c67e2e4
Bianca <biancaridesouza@gmail.com>
2022-02-03 20:03:54 -0300

    Finish

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,43 +1,41 @@
-import { useSession, signIn } from 'next-auth/react';
-import { api } from '../../services/api';
-import { getStripeJs } from '../../services/stripe-js';
-import styles from './styles.module.scss';
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+import styles from './styles.module.scss'
 
-interface SubscribeButtonProps {
-    priceId: string;
-}
-
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-
-    const { data: session } = useSession();
-    
-    async function handleSubscribe() {
-
-        if (!session) {
-            signIn('github');
-            return;
-        }
-
-        try {
-
-            const response = await api.post('/subscribe');
-            const { sessionId } = response.data;
-            const stripe = await getStripeJs();
-
-            await stripe.redirectToCheckout({ sessionId }); //
-
-        } catch (err) {
-            alert(err.message);
-        }
-
+export function SubscribeButton() {
+  const router = useRouter()
+  const { data: session } = useSession()
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
     }
 
-    return (
+    if (session.activeSubscription) {
+      router.push('/posts')
+      return
+    }
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
 
-        <button type="button" className={styles.subscribeButton} onClick={() => handleSubscribe()} >
-            Subscribe now
-        </ button>
+      const stripe = await getStripeJs()
 
-    );
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
 
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  )
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,11 @@
-import { query as q } from 'faunadb';
-
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-
-import { fauna } from '../../../services/fauna';
-
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna'
+import { query as q } from 'faunadb'
+import { signIn } from 'next-auth/react'
 export default NextAuth({
-
+  secret: process.env.NEXTAUTH_SECRET,
+  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -17,50 +16,60 @@ export default NextAuth({
         },
       },
     }),
+    // ...add more providers here
   ],
-
   callbacks: {
-    async signIn({user, account, profile}) {
-
-      const { email } = user;
-      
+    async session({ session }) {
       try {
-
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index('subscription_by_status'), 'active'),
+            ])
+          )
+        )
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        }
+      } catch (err) {
+        return {
+          ...session,
+          activeSubscription: null,
+          err,
+        }
+      }
+    },
+    async signIn({ user, account, profile }) {
+      const { email } = user
+      try {
         await fauna.query(
-
           q.If(
             q.Not(
               q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
               )
             ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email }}
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(user.email)
-              )
-            )
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
           )
-
         )
-
-        return true;
-
+        return true
       } catch {
-
-        return false;
-
+        return false
       }
-
-
-    }
-  }
-
+    },
+  },
 })
